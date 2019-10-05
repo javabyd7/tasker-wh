3de9428fd0f48e2a5c0f1b8af1a9798d8beec900
Arkadiusz Partyka <partyka.arkadiusz@gmail.com>
2019-10-05 11:46:29 +0200

    - introduce tasks-form

--- src/main/java/pl/sda/task/mvc/MvcTasksController.java
+++ src/main/java/pl/sda/task/mvc/MvcTasksController.java
@@ -1,6 +1,8 @@
 package pl.sda.task.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.task.model.Task;
@@ -16,9 +18,20 @@ public class MvcTasksController {
         this.taskService = taskService;
     }
 
+    @GetMapping("/addTask")
+    public String getAddTask() {
+        return "createTask-form";
+    }
+
     @PostMapping("/addTask")
     public String addTask(Task task) {
         taskService.create(task);
         return "redirect:/mvc/tasks";
+    }
+
+    @GetMapping("/tasks")
+    public String getTasks(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "tasks-form";
     }
 }
--- src/test/java/pl/sda/task/MvcTaskControllerIntegrationTest.java
+++ src/test/java/pl/sda/task/MvcTaskControllerIntegrationTest.java
@@ -1,31 +1,39 @@
 package pl.sda.task;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.sda.task.model.Task;
 import pl.sda.task.mvc.MvcTasksController;
 import pl.sda.task.service.TaskService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MvcTasksController.class)
-public class MvcAddTaskControllerIntegrationTest {
+public class MvcTaskControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
     @MockBean
     TaskService taskService;
 
-    @DisplayName("When post on /mvc/addTask with task parameters then task will be created")
+    @DisplayName("When POST on /mvc/addTask with task parameters then task will be created")
     @Test
     void test() throws Exception {
         //when
@@ -36,6 +44,20 @@ public class MvcAddTaskControllerIntegrationTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/mvc/tasks"));
         verify(taskService, times(1)).create(any(Task.class));
+    }
 
+    @DisplayName("When GET on /mvc/tasks then all tasks should be returned")
+    @Test
+    void test1() throws Exception {
+        //given
+        Collection<Task> tasks = Arrays.asList(new Task());
+        when(taskService.findAll()).thenReturn(tasks);
+        //when
+        mockMvc.perform(get("/mvc/tasks"))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(view().name("tasks-form"))
+                .andExpect(model().attribute("tasks", tasks))
+                .andExpect(content().string(containsString("<title>Tasks</title>")));
     }
 }
